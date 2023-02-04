81f5c27df0f5ca5eee4deff1ee9edc0646a98c41
KingAshu <111638787+ashu7970@users.noreply.github.com>
2023-02-04 08:58:14 +0530

    Add files via upload

---
+++ Prime1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Prime1
+{
+    public static void main(String[] args)
+
+    {
+        Scanner s1 = new Scanner(System.in);
+
+        System.out.print("Enter a number = ");
+        int a = s1.nextInt();
+
+        for (int i = 2; i <= a; i++)
+        {
+            if (a % i == 0 && a%++i == 0 )
+            {
+                System.out.println("the entered number is not a prime number");
+                break;
+            }
+            else
+            {
+                System.out.println("the entered number is prime number");
+                break;
+            }
+
+        }
+
+
+    }
+}
